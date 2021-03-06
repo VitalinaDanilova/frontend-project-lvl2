ae6ce625a470ac0ac964200bc3a2d2cb94e78d0e
Vitalina Danilova <haalvmorke@gmail.com>
2021-03-06 17:34:59 +0300

    update description

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import program from '../src/index.js';
+import genDiff from '../src/index.js';
 
-console.log(program);
+genDiff();
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import { Command } from 'commander/esm.mjs';
 
+const genDiff = () => {
+  const program = new Command();
 
-const program = new Command();
+  program
+    .arguments('<filepath1> <filepath2>')
+    .description('Compares two configuration files and shows a difference.')
+    .option('V, --version', 'output the version number')
+    .option('-f, --format [type]', 'output format')
+    .action((options) => {
+      console.log(options.version);
+      console.log(options.help);
+    });
 
-program
-  .description('Compares two configuration files and shows a difference.')
-  .option('V, --version', 'output the version number')
-  .action((options) => {
-    console.log(options.version);
-    console.log(options.help);
-});
+  program.parse(process.argv);
+  return program;
+};
 
-program.parse(process.argv);
-
-export default program;
+export default genDiff;
